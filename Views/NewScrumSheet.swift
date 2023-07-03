e098da9c885d8cdeb36c2286b71bb9358f8ef7d3
Damian <bzdyrad@gmail.com>
2023-07-03 14:56:48 -0400

    Complete Updating App Data

---
+++ Views/NewScrumSheet.swift
@@ -0,0 +1,40 @@
+//
+//  NewScrumSheet.swift
+//  ScrumDinger
+//
+//  Created by Damian Bzdyra on 7/3/23.
+//
+
+import SwiftUI
+
+struct NewScrumSheet: View {
+    @Binding var isPresentingNewScrumView: Bool
+    @Binding var scrums: [DailyScrum]
+    @State private var newScrum = DailyScrum.emptyScrum
+    
+    var body: some View {
+        NavigationStack {
+            DetailEditView(scrum: $newScrum)
+                .toolbar {
+                    ToolbarItem(placement: .cancellationAction){
+                        Button("Dismiss") {
+                            isPresentingNewScrumView = false
+                        }
+                    }
+                    
+                    ToolbarItem(placement: .confirmationAction){
+                        Button("Add") {
+                            scrums.append(newScrum)
+                            isPresentingNewScrumView = false
+                        }
+                    }
+                }
+        }
+    }
+}
+
+struct NewScrumSheet_Previews: PreviewProvider {
+    static var previews: some View {
+        NewScrumSheet(isPresentingNewScrumView: .constant(true), scrums: .constant(DailyScrum.sampleData))
+    }
+}
